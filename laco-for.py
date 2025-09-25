78c4c1c85c38f507e9218b7f669d37428d18a534
Cristoferhaki <cristofer.chaves@escola.pr.gov.br>
2025-09-25 11:26:17 -0300

    Create  laco-for.py

---
+++ laco-for.py
@@ -0,0 +1,6 @@
+https://www.programiz.com/online-compiler/4O0mXpvsNKHFu
+https://www.programiz.com/online-compiler/3HhbME500L3j4
+https://www.programiz.com/online-compiler/50mo1uhDsUFij
+https://www.programiz.com/online-compiler/9RcfvMbfjCigT
+https://www.programiz.com/online-compiler/7Gwjk9mggNd4p
+https://www.programiz.com/online-compiler/17DnlCYRIOHSN
